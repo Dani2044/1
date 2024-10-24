817a48df07d2775284609e13c8d9e2cada824ed9
Daniel Castro <castro-df@javeriana.edu.co>
2024-10-24 01:48:14 +0000

    Created User class

--- match/src/main/java/com/javematch/match/model/Usuario.java
+++ match/src/main/java/com/javematch/match/model/Usuario.java
@@ -1,5 +1,17 @@
 package com.javematch.match.model;
 
-public class Usuario {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
+public class Usuario {
+    @Id
+    private Long idUsuario;
+    private String nombre;
+    private int edad;
+    private Boolean genero; // Como debe ser
+    private String ubicacion;
+    private String biografia;
 }
